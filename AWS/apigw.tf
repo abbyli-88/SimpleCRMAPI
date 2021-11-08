49debc4a6ecef6edd2222eb9f61e65d52d1b0630
liuky008@gmail.com <liuky008@gmail.com>
2021-11-08 21:11:42 +1100

    menabeld cloudwatch data_trace_enabled for API Gateway

--- AWS/apigw.tf
+++ AWS/apigw.tf
@@ -60,11 +60,9 @@ resource "aws_api_gateway_integration_response" "integration_response" {
 
 resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway_rest.id
-  # stage_name  = var.api_deploy_stage_name
-
-  #depends_on = [
-  #  aws_api_gateway_integration.api_gateway_integration
-  #]
+  depends_on = [
+    aws_api_gateway_method.api_operation
+  ]
   lifecycle {
     create_before_destroy = true
   }
@@ -88,5 +86,6 @@ resource "aws_api_gateway_method_settings" "all" {
   settings {
     metrics_enabled = true
     logging_level   = "INFO"
+    data_trace_enabled = true
   }
 }
